59601bef4a54366f9d4f2603d9a0905d3ca31eb2
Dmitry Zhigalo <84251312+dmt-zh@users.noreply.github.com>
2021-11-08 15:39:38 +0300

    Add files via upload

---
+++ Stats and R/211108_main_stat.R
@@ -0,0 +1,15 @@
+# Воспользуемся данными diamonds из библиотеки ggplot2. При помощи критерия Хи - квадрат проверьте гипотезу о 
+# взаимосвязи качества огранки бриллианта (сut) и его цвета (color). В переменную main_stat сохраните значение
+# статистики критерия Хи - квадрат. Обратите внимание, main_stat должен быть вектором из одного элемента, а не
+# списком (листом).
+
+library(ggplot2)
+d_table <- table(diamonds$cut, diamonds$color)
+main_stat <- chisq.test(d_table)$statistic
+
+
+# Pearson's Chi-squared test
+# data:  d_table
+# X-squared = 310.32, df = 24, p-value < 2.2e-16
+
+# Нет оснований отклонить нулевую гипотезу
