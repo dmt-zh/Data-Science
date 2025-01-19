867f97e2f3b3f2438b9770f26ce827214f1a2465
Dmitry Zhigalo <84251312+dmt-zh@users.noreply.github.com>
2025-01-19 11:07:58 +0300

    Create nn_344.py

---
+++ PyTorch/nn_344.py
@@ -0,0 +1,34 @@
+# Дана структура нейронной сети (см. на картинке). Такие блоки используются в архитектуре ResNet18 и ResNet34.
+# Данная схема отличается от блоков в ResNet только кол-вом каналов.
+
+# В этом упражнении:
+# Создайте класс с названием BasicBlock, с помощью которого можно создать нейронную сеть, как на картинке.
+# В данном примере skip connection содержит свёрточный слой. Данный слой называется downsample.
+# Создайте модель нейронной сети, на вход которой подается тензор с 10-ю каналами, а на выходе получается тензор с 10-ю каналами.
+# Результат запишите в переменную block.
+
+
+import torch
+from torch import nn
+
+class BasicBlock(nn.Module):
+    def __init__(self, inp_channels, out_channels):
+        super().__init__()
+        self.main_conv_layer = nn.Sequential(
+            nn.Conv2d(inp_channels, 10, (3, 3), padding=1, stride=2, bias=False),
+            nn.BatchNorm2d(10),
+            nn.ReLU(),
+            nn.Conv2d(10, out_channels, (3, 3), padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+        )
+        self.branch_conv_layer = nn.Sequential(
+            nn.Conv2d(inp_channels, out_channels, (1, 1), stride=2, bias=False),
+            nn.BatchNorm2d(out_channels),
+        )
+        self.activation = nn.ReLU()
+
+    def forward(self, batch):
+        outputs = self.main_conv_layer(batch) + self.branch_conv_layer(batch)
+        return self.activation(outputs)
+
+block = BasicBlock(10, 10)
