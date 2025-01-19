947781ea0ab510d24f9c5786bbf0f08f4b2891c8
Dmitry Zhigalo <84251312+dmt-zh@users.noreply.github.com>
2025-01-19 10:45:05 +0300

    Create nn_342.py

---
+++ PyTorch/nn_342.py
@@ -0,0 +1,25 @@
+# В этом упражнении:
+# Создайте класс с названием MyModel, с помощью которого можно создать нейронную сеть, как на картинке.
+# Создайте модель нейронной сети, на вход которой подается тензор с 5-ю каналами, а на выходе 
+# получается тензор с 10-ю каналами. Результат запишите в переменную model.
+
+
+import torch
+from torch import nn
+
+class MyModel(nn.Module):
+    def __init__(self, inp_channels, out_channels):
+        super().__init__()
+        self.layer = nn.Sequential(
+            nn.Conv2d(inp_channels, 10, (1, 1), bias=False),
+            nn.BatchNorm2d(10),
+            nn.ReLU(),
+            nn.Conv2d(10, out_channels, (3, 3), padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+        )
+
+    def forward(self, batch):
+        return self.layer(batch)
+
+model = MyModel(5, 10)
